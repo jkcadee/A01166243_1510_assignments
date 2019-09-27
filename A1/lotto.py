4d89647e19265a751d672aa4bb9c58ec54fd273d
Janelle Kwok <janellesertyankwok@gmail.com>
2019-09-26 21:46:43 -0700

    Added docstrings and comments to number_generator and main

--- A1/lotto.py
+++ A1/lotto.py
@@ -2,13 +2,25 @@ import random
 
 
 def number_generator():
+    """Creates six random numbers and orders them from lowest to highest"""
     length = 6
     set_of_six_numbers = random.sample(range(1, 49), length)
+# Creating the list of six numbers
     set_of_six_numbers.sort()
     return set_of_six_numbers
 
 
+"""
+Return a list of specified length with numbers between 1 and 49 going from lowest to highest.
+
+:precondition: Length of the list must be specified
+:postcondition: A list containing the amount of numbers specified in the length variable
+:return: A list that is the specified length with numbers between 1 and 49, sorted from lowest to highest.
+"""
+
+
 def main():
+    """Executes the program"""
     print(number_generator())
 
 
