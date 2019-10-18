4f3570587e0cf4001da71d912f9eb42a297ce61a
Jkcadee <janellesertyankwok@gmail.com>
2019-10-17 22:53:10 -0700

    helpful computing club stuff

--- A2/test_choose_inventory.py
+++ A2/test_choose_inventory.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 from A01166243_1510_assignments.A2.dungeonsanddragons import choose_inventory
 from unittest.mock import patch
+import sys
 import io
 
+
 class TestChooseInventory(TestCase):
-    def test_choose_inventory_print_list(self):
+    def test_choose_inventory_print_(self):
         output = io.StringIO()
         sys.stout = output
+        choose_inventory()
 
 
