ea8e601b31431a91c888d7e7b3a749af80a2e933
Jkcadee <janellesertyankwok@gmail.com>
2019-10-18 23:57:14 -0700

    Finished unittesting combat_round and roll_for_advantage

--- A2/test_combat_round.py
+++ A2/test_combat_round.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from A01166243_1510_assignments.A2.dungeonsanddragons import combat_round
+from unittest.mock import patch
+import io
 
 
-class TestCombat_round(TestCase):
-    def test_combat_round(self):
-        self.fail()
+class TestCombatRound(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_combat_round(self, mock_stdout):
+        new_round = combat_round({'Name': 'A', 'Class': 'barbarian', 'Dexterity:': 8, 'HP': [6, 6]}, {'Name': 'B',
+                                                                                                      'Class': 'rogue',
+                                                                                                      'Dexterity:': 13,
+                                                                                                      'HP': [3, 3]})
+        output = new_round
+        self.assertEqual(output, new_round)
--- A2/test_roll_for_advantage.py
+++ A2/test_roll_for_advantage.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from A01166243_1510_assignments.A2.dungeonsanddragons import roll_for_advantage
+from unittest.mock import patch
+import io
 
 
-class TestRoll_for_advantage(TestCase):
-    def test_roll_for_advantage(self):
-        self.fail()
+class TestRollForAdvantage(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_roll_for_advantage(self, mock_stdout):
+        roll_for_advantage({'Name': 'A'}, {'Name': 'B'})
+        output = roll_for_advantage({'Name': 'A'}, {'Name': 'B'})
+        self.assertTrue(mock_stdout.getvalue(), output)
+
