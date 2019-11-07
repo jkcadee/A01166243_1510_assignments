26ac37248832d38b5d435039bf74748d486745c5
Jkcadee <janellesertyankwok@gmail.com>
2019-11-07 13:59:53 -0800

    Created unittests for check_exit_reached, display_board and validate_move

--- A3/SUD.py
+++ A3/SUD.py
@@ -1,6 +1,6 @@
 import random
-import A01166243_1510_assignments.A3.character
-import A01166243_1510_assignments.A3.monster
+from A01166243_1510_assignments.A3.character import *
+from A01166243_1510_assignments.A3.monster import *
 
 
 def roll_die(rolls, sides):
@@ -13,28 +13,61 @@ def roll_die(rolls, sides):
 
 
 def create_board(row_size, col_size):
-    space = 'x'
     playing_board = []
     for row in range(row_size):
         sub_list = []
         for column in range(col_size):
-            sub_list.append(space)
+            sub_list.append((column, row))
         playing_board.append(sub_list)
     return playing_board
 
 
-# def player_movement(board, move):  # List (board)
-#     for row in range(len(board)):
-#         for column in range(len(board[row])):
+def user_choice():
+    """
+    Return a user input of N, S, E, W.
+
+    :precondition: Input must be N, S, E, W.
+    :postcondition: Returns the input capitalized.
+    :return: The input capitalized.
+    """
+    move = input('Where would you like to move? (Use N, S, E, W to move North, South, East or West.) \n')
+    return move.capitalize()
 
 
-def display_board(play_board):
+def player_movement(character_coordinates: list, maximum_limit: int, current_position: int, direction: str) -> list:
+    if direction in ('N', 'W'):
+        if character_coordinates[current_position] != 0:
+            character_coordinates[current_position] -= 1
+    elif direction in ('S', 'E'):
+        if character_coordinates[current_position] != maximum_limit - 1:
+            character_coordinates[current_position] += 1
+    return character_coordinates
+
+
+def display_board(play_board: list, character: dict):
+    character_key = character['Position:']
     for index in range(len(play_board)):
         for index_two in range(len(play_board[index])):
-            if index_two == len(play_board[index]) - 1:
-                print(play_board[index][index_two])
+            character_current_position = [index, index_two]
+            if character_current_position == character_key:
+                if index_two == len(play_board[index]) - 1:
+                    print('c')
+                else:
+                    print('c', end=" ")
             else:
-                print(play_board[index][index_two], end=" ")
+                if index_two == len(play_board[index]) - 1:
+                    print('x')
+                else:
+                    print('x', end=" ")
 
 
-display_board(create_board(5, 5))
+def game():
+    display_board(create_board(5, 5), player_character(roll_die(1, 3), roll_die(1, 3)))
+
+
+def main():
+    game()
+
+
+if __name__ == '__main__':
+    main()
--- A3/character.py
+++ A3/character.py
@@ -2,17 +2,22 @@ def generate_name():
     return input('What\'s your name?: ')
 
 
-def player_character():
+# def character_coords(x_coord: int, y_coord: int) -> list:
+#     return [x_coord, y_coord]
+
+
+def player_character(x_coord: int, y_coord: int) -> dict:
     the_character = {'Name:': generate_name(),
-                     'Style Level: ': [10, 10],  # SL list: 1st value is max SL, second is current SL
-                     'Inventory: ': []}
+                     'Style Level:': [10, 10],  # SL list: 1st value is max SL, second is current SL
+                     'Inventory:': [],
+                     'Position:': [x_coord, y_coord]}
     return the_character
 
 
 def print_character(a_character):  # Must always be a dictionary
     for stat_name, stat_info in a_character.items():
         if isinstance(stat_info, list):
-            stat_list = stat_name
+            stat_list = stat_name + ' '
             for index in range(0, len(stat_info)):
                 stat_list += str(stat_info[index])
                 if len(stat_info) - 1 > index:
@@ -22,4 +27,4 @@ def print_character(a_character):  # Must always be a dictionary
             print(stat_name, stat_info)
 
 
-print_character(player_character())
+# print_character(player_character())
