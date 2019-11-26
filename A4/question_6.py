94494e8bf9e7283eb4150255ad22e50eb3a3b992
Jkcadee <janellesertyankwok@gmail.com>
2019-11-26 08:26:39 -0800

    Completed all unittests

--- A4/question_6.py
+++ A4/question_6.py
@@ -1,18 +1,27 @@
+# Each unittest tests and different equivalency partition. These are 6 of the ones I could think of if I could only
+# write 6.
+
+# Testing the function when both parameters are positive lines and have no intersection.
 # def test_line_intersect_no_intersection_pos(self):
 #     self.assertEqual(None, line_intersect([[0.0, 0.0], [1.0, 3.0]], [[1.0, 0.0], [2.0, 3.0]]))
 
+# Testing the function when both parameters are positive lines and are coincident.
 # def test_line_intersect_coincident_pos(self):
 #     self.assertEqual([[0.0, 0.0], [1.0, 3.0]], line_intersect([[0.0, 0.0], [1.0, 3.0]], [[0.0, 0.0], [1.0, 3.0]]))
 
+# Testing the function when both parameters are positive lines and have one intersection.
 # def test_line_intersect_one_intersect_pos(self):
 #     self.assertEqual([0.25, 0.75], line_intersect([[0.0, 0.0], [1.0, 3.0]], [[1.0, 0.0], [0.0, 1.0]]))
 
+# Testing the function when both parameters are negative lines and have no intersection.
 # def test_line_intersect_no_intersection_negative(self):
 #     self.assertEqual(None, line_intersect([[-1.0, -1.0], [-2.0, -3.0]], [[-2.0, -1.0], [-3.0, -3.0]]))
 
+# Testing the function when both parameters are positive lines are coincident.
 # def test_line_intersect_coincident_negative(self):
 #     self.assertEqual([[-1.0, -1.0], [-2.0, -3.0]], line_intersect([[-1.0, -1.0], [-2.0, -3.0]], [[-1.0, -1.0],
 #                       [-2.0, -3.0]]))
 
+# Testing the function when both parameters are positive lines and have one intersection.
 # def test_line_intersect_one_intersect_negative(self):
 #     self.assertEqual([-1.0, -1.0], line_intersect([[-1.0, -1.0], [-2.0, -3.0]], [[-2.0, -1.0], [-1.0, -1.0]]))
