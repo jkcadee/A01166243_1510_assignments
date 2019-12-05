c05ebb786ba3d0827641d2a806ae3ffcc2551e20
Jkcadee <janellesertyankwok@gmail.com>
2019-12-04 23:25:24 -0800

    Finalized refactoring

--- A5/A5_Sub_Pub.py
+++ A5/A5_Sub_Pub.py
@@ -4,18 +4,21 @@ import paho.mqtt.publish as publish
 # All these variables are constants
 BROKER_JAMES = "192.168.43.8"
 BROKER_JANELLE = "192.168.43.139"
-BROKER_JUSTIN = "192.168.43."
+BROKER_JUSTIN = "192.168.43.54"
 
 MQTT_TOPIC_JAMES = "James"
 MQTT_TOPIC_JANELLE = "Janelle"
 MQTT_TOPIC_JUSTIN = "Justin"
 
 
-def on_connect(client, rc) -> None:
+def on_connect(client, userdata, flags, rc) -> None:
     """Subscribe to the MQTT topic and print the result code.
 
     :param client: MQTT Client.
     :precondition: Must be a valid MQTT Client.
+    :param userdata: Any definable python data type.
+    :param flags: Response flags sent to broker.
+    :precondition: Must be a dictionary. Contains rc.
     :param rc: RC code.
     :precondition: Must be a valid RC code.
     :postcondition: Subscribe to different topics and print useful messages.
@@ -27,17 +30,18 @@ def on_connect(client, rc) -> None:
     client.subscribe(MQTT_TOPIC_JUSTIN)
     
     
-def on_message(client, msg) -> None:
+def on_message(client, userdata, msg) -> None:
     """Print the published message,
 
     :param client: MQTT Client.
     :precondition: Must be a valid MQTT Client.
+    :param userdata: Any definable python data type.
     :param msg: The message in a string.
     :precondition: Must be a valid string message.
     :postcondition: Print the published message.
     :return: None.
     """
-    print(msg.topic+" "+str(msg.payload.decode("utf-8")))  # Receiving the published message from different pis
+    print(msg.topic+":"+" "+str(msg.payload.decode("utf-8")))  # Receiving the published message from different pis
 
 
 def main():
@@ -45,17 +49,16 @@ def main():
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    try:
-        client.connect(BROKER_JANELLE, 1883, 60)
-    except ConnectionError:
-        raise ConnectionError
-    else:
-        inputted_message = ''
-        while inputted_message != 'Quit':  # Messages will be allowed to be sent until 'Quit' is inputted
-            client.loop_start()
-            inputted_message = input()
-            publish.single(MQTT_TOPIC_JANELLE, inputted_message, hostname=BROKER_JAMES)
+    inputted_message = ''
+    client.connect(BROKER_JANELLE, 1883, 60)
+    while inputted_message != 'Quit':  # Messages will be allowed to be sent until 'Quit' is inputted
+        client.loop_start()
+        inputted_message = input()
+        try: 
+            #publish.single(MQTT_TOPIC_JANELLE, inputted_message, hostname=BROKER_JAMES)
             publish.single(MQTT_TOPIC_JANELLE, inputted_message, hostname=BROKER_JUSTIN)
+        except OSError:
+            raise OSError('There is no route to the host!')
 
 
 if __name__ == "__main__":
